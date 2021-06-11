41886a4076bfd0731fc57b5d82a8a25a90c8a9ee
dwkang92 <dwkang921@gmail.com>
2021-06-11 22:05:44 +0900

    simple input

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,23 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const styles = {
+    border: '1px solid #eee',
+    padding: '20px',
+    display: 'felx',
+    width: '100vw',
+    maxWidth: '400px',
+    margin: '30px auto',
+    flexDirection: 'column'
+  };
+  const textA = {
+    textAlign: 'left',
+    color: 'red'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+     <h1 style={{color: 'skyblue'}}>Good Day!</h1>
+     <hr style={{width: '100%'}}></hr>
+     <p style={textA}>fill in your name below.</p>
+     <input type="text"></input>
+     </div>
     </div>
   );
 }
